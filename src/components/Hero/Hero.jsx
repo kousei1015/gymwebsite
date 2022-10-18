5b465372e47d3a18200d72cff675e9aadd958bcd
kousei1015 <wtks39176@gmail.com>
2022-10-18 23:25:57 +0900

    Programs and Reasons completed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Header from "../Header/Header";
 import "./Hero.css";
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
+
 const Hero = () => {
   return (
     <div className="hero">
@@ -21,7 +26,6 @@ const Hero = () => {
             <span>Ideal body</span>
             <span></span>
           </div>
-
           <div>
             <span>
               In here we will help you to shape and build Ideal body and live up
@@ -29,8 +33,53 @@ const Hero = () => {
             </span>
           </div>
         </div>
+
+        {/* figures  */}
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>export coachs</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+        {/* button */}
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
       </div>
-      <div className="right-h">right side</div>
+
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+
+        {/*hero images */}
+        <img src={hero_image} className="hero-image" alt="" />
+        <img src={hero_image_back} className="hero-image-back" alt="" />
+
+        {/*calories */}
+        <div className="calories">
+          <img src={Calories} alt="" />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
+
+        
+      </div>
     </div>
   );
 };
